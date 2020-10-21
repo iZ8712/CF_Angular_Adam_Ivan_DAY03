9ce033f2a66d21d65de87e97c8e9e9a9b9736276
AdamAl-Salehi <email@example.combakr.arkan@icloud.com>
2020-10-21 13:29:10 +0200

    added products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NavbarComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
